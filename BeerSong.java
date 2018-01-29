aef747c683e5dd9b462520ed68d467bc2be71ec9
Joshua Mullins <jjmullins@oakland.edu>
2018-01-29 02:05:25 -0500

    Program that prints the 99 bottles of beer song

---
+++ BeerSong.java
@@ -0,0 +1,24 @@
+public class BeerSong {
+    public static void main(String[] args) {
+        int beerNum = 99;
+        String word = "bottles";
+
+        while (beerNum > 0) {
+
+            System.out.println(beerNum + " " + word + " of beer on the wall, " + beerNum + " " + word + " of beer");
+            beerNum = beerNum - 1;
+
+            if (beerNum == 1) {
+                word = "bottle";
+            }
+
+            if (beerNum > 0) {
+                System.out.println("Take one down, pass it round " + beerNum + " " + word + " of beer on the wall.");
+            }
+        }
+
+        if (beerNum == 0) {
+            System.out.println("No more bottles of beer on the wall.");
+        }
+    }
+}
